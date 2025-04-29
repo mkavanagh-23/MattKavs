2afff8f4001dde09918ccfe3fd8b49f230c679d3
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-04-28 20:25:52 -0400

    Update build script logic

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# Build the library
 cd build/
 echo "Building C++ Library 'MattKavs'"
 cmake ..
 make
 
+# Run the tests
 echo "Running tests..."
 ctest
+
+# Install the library
+if [ $? -eq 0 ]; then
+  read -p "Would you like to install the library to your system? (y/n): " choice
+  if [ "$choice" == "Y" ] || [ "$choice" == "y" ]; then
+    sudo make install
+  fi
+fi
