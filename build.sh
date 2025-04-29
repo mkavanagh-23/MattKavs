88041363da6d0180eb304c8bd7f3619eaa7d1c90
Matthew Kavanagh <matt@matthewkavanagh.us>
2025-04-29 12:42:30 -0400

    Update system library install paths

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@
 mkdir build
 cd build/
 echo "Building C++ Library 'MattKavs'"
-cmake ..
+
+# Set install prefix to /usr to avoid needing -I /usr/local/include and -L /usr/local/lib
+cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make
 
 # Run the tests
@@ -14,7 +16,7 @@ make
 # Install the library
 if [ $? -eq 0 ]; then
   read -p "Would you like to install the library to your system? (y/n): " choice
-  if [ "$choice" == "Y" ] || [ "$choice" == "y" ]; then
+  if [[ "$choice" =~ ^[Yy]$ ]]; then
     sudo make install
   fi
 fi
